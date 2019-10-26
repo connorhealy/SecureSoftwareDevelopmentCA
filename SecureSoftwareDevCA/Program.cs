85e1a46240b7245b2a71f40e411f6a98107e839b
Connor <s00172979>
2019-10-26 14:25:02 +0100

    added menu for login

--- SecureSoftwareDevCA/Program.cs
+++ SecureSoftwareDevCA/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SecureSoftwareDevCA
 {
@@ -10,6 +11,68 @@ namespace SecureSoftwareDevCA
     {
         static void Main(string[] args)
         {
+            int num1 = 0; int num2 = 0;
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Select user to log in as:");
+            Console.WriteLine("1 - Admin");
+            Console.WriteLine("2 - User");
+           
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine($"Enter admin password:");
+                    break;
+                case "2":
+                    Console.WriteLine($"Enter user password:");
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection");
+                    break;
+            }
+           
         }
+
+        public void AdminLogin()
+        {
+            string password = 
+            while()
+        }
+        //string username;
+        //Console.WriteLine("log in:");
+        //Console.WriteLine("username:");
+        //username = Console.ReadLine();
+
+        //Console.WriteLine(username);
+
+        //List<string> usernames = new List<string>();
+        //List<string> passwords = new List<string>();
+
+        //using (var reader = new StreamReader(@"users.csv"))
+        //{
+
+        //    while (!reader.EndOfStream)
+        //    {
+        //        var line = reader.ReadLine();
+        //        var values = line.Split(',');
+
+        //        usernames.Add(values[0]);
+        //        passwords.Add(values[1]);
+        //    }
+        //}
+
+        //usernames.ForEach(single_username =>
+        //{
+        //    Console.WriteLine(single_username);
+        //});
+
+        //passwords.ForEach(single_password =>
+        //{  
+        //    Console.WriteLine(single_password);
+        //});
+
+
+
     }
+
 }
---
+++ SecureSoftwareDevCA/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureSoftwareDevCA
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        public Boolean IsAdmin { get; set; }
+
+    }
+}
